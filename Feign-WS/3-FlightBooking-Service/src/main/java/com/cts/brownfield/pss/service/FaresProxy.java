f5634feeb4a90c35f606cf111b6244efa2ad1d68
richardsforu <praveen.somireddy@gmail.com>
2020-09-03 15:11:24 +0530

    cloud-services

---
+++ Feign-WS/3-FlightBooking-Service/src/main/java/com/cts/brownfield/pss/service/FaresProxy.java
@@ -0,0 +1,16 @@
+package com.cts.brownfield.pss.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cts.brownfield.pss.entity.Fares;
+
+@FeignClient(name="fare-service",url="http://localhost:8081/api/pss")
+public interface FaresProxy {
+	@GetMapping(value = "/getFare/{id}")
+	Fares getFare(@PathVariable("id") long id);
+
+	
+
+}
